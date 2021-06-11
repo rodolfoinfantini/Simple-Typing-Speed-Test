811e0ecf32d3641f8e5738a7ebff27a0f597c41b
SirVoRaK <sirvoraky@gmail.com>
2021-06-11 14:17:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+var words = "uma não dessa não não é dessa e discordo de Um fosse propósito que não barriga é e acidentes mais do o é pior de meio hoje ímpar principalmente importa separado Evite unido diminutivo vêm para de e o vale na que bicicleta sempre mim bois Há se pegou era Eu piranhas exceção hora coisa um concordo Moisés é tarde que Daqui uma importantes Uma o que vivo porque e do não acidentes dê nadar 0 hoje mais sucedem propósito serão regra são Em serão golfinho mais estará discordo principalmente contrário que hoje hoje um Um contigo chifre se Os uma A tinha dos é certa andou uma discordo Em nunca nadar pode para ainda Por você andou pode do O males é quem não Pobre bode planetas é algarismos de tem linha diminutivo a O O Pobre porque barriga é e dois que ninguém quer piranhas queijadinha Em vêm de Daqui mais coisa barriga bois É exceção não importante primeiros Evite que quer caramba O morre Qual vai sei A acidentes vai O não que respeito A sucessão número contrário algarismos pra não ninguém mais sucessão dos dos diminutivo vivo vem Só que mais sem Essas afogado ímpar Evite importa com mão afogado Moisés principalmente bicicleta O fosse azul Agora não Não do vida sem pouco contrário maria-mole outra bom o sem últimos para pra planetas quer tarde causa Eu é vivo são pra no do então se chuta salva é gente pra acidentes A uiva motorista é Um caro que queijadinha do é Eu se lobo Vermelho que eu Jesus A chuta maria-mole regra vem males que mão mim que mais afogado mais você vida o na jacaré esteja a pelo O uma meio andas a mão que caro que do frente caramba sucedem vai volto sonho tudo respeito andou estará que número Qual Só dessa comendo o caramba o não hora acabou estará Toda Se acidentes A vale e que primeiros que sem concordo LSD exceção o cessar é importante romanos da chifre está direi que do que pássaro pássaro piranhas tem importante Toda 0 Como principalmente uma cobrador… droga passageiro e costas regra menos males respeito o coisas O é vida certa O Por exceção uma que mulher importa estará nem golfinho dinheiro mas importa compro Sonhar e os Há documento o sai vai sai uiva Se fosse vivo lobo seu do primeiros voando E e voando e que pouco que importa é mosca regra linha com vida esteja e é para Um é causa A voando O estará o não mais direi tarde Antes pássaro amanhã O o só que é que 0 Eu para principalmente que estará a Moisés vai A Terra escreve maria-mole tinha sucessivos nasce que Evite do que dessa em nada vivo coisa e não coisa passa caramba exceção importante Só elefante povo caro o pode Agora Uma um tudo é acidentes bois o Toda mim única seu chifre chifre Por o que de passa frente dois maria-mole não diminutivo acidentes travesseiro Em meio caramba ninguém vida quer Toda serão dessa ninguém azul Terra dessa quem frente sem álcool caramba coisa se e que regra escreve TVs elefante feliz é vivo que salva que Em concordo Eu o mão não de mais Faça morre fosse Sonhar número é de lobo hora do você mim uma circo sem exceção meio primeiros A A A a acidentes pior caro caramba que hoje maria-mole Há enche sem 0 e o nasce circo salva tarde 0 Pobre unido mais rio única quer afogado nem a coisa Agora o escreve enche O jacaré te de sol hora gente quer pra Vermelho vou mais exceção Não estará o O do do hoje TVs que jacaré é os nasce Há andou maior de e sonho unido coisas sem amanhã algarismos certa afogado de piranhas é do de Essas lá de não homem que mundo elas planetas gente discordo mais só acidentes mim vou escreve voando urso o coisa no outra e hoje Toda homem Os porque sempre o a dinheiro mas droga que é Vermelho uma fosse de Evite quem sonho Evite sucedem voando meio vai é você meio coisa Daqui Moisés vida do acordar é te bode é são golfinho Daqui é A de documento o sei Em que mais ninguém Que que piranhas Só é sucessivos tem se Só que compro Antes tamanho Na que enche TVs pouco quanto estará é A vida diminutivo a pássaro salva coisas tamanho vai O do esteja que o droga os mais Daqui principalmente coisa importantes do ímpar de TVs que males de volta Essas escreve cessar pássaro é vêm coisas mais temos pra coisa muita na nem é conhecidos sucessão do pegou andas junto respeito importa de coisa caramba dessa eu A sem Não que não pior mais mais são sol dois tem não Mais era povo propósito tarde dessa eu sai maria-mole não de dessa Toda uma costas gente dessa Por respeito é Evite discordo o gente jacaré e propósito e coisa coisa sem que bicicleta exceção azul se Essas sem em vale ímpar contigo O Uma separado Há não inimigos linha E nadar contigo existo e que gente motorista unido tinha frente Daqui acordar quer e coisa pra ímpar 0 Como quanto que covarde ninguém uma 0 significar nunca e O hoje sai a droga homem não do que A volto junto pior ninguém barriga O pra é contigo mim principalmente escreve são A sempre certa Por mulher que mais mim existo Essas hora 0 Jesus do são pressa males na lá que caramba Uma hoje elefante serão tem panda que gente coisa mais mulher sem hoje queijadinha Uma nem era sempre barriga sai é dessa maria-mole A coisa pegou é sem Diz-me uma pra que um dê o é do o o males tamanho pássaro azul Onde é Essas dos Pobre do propósito o hoje coisa que passa importante Por nem lá concordo tarde coisas O dessa regra maria-mole 0 é Terra tarde quanto morre dessa vai te acabou sei vivo O te sucessão vida principalmente gigante Os quer O pouco tarde Evite mais tem salva Daqui uiva Jesus Não vêm nunca dessa mim tem que a que vida de documento mulher de está você Qual eu osso Uma caramba dos pelo no concordo chuta enche Como no Onde males O Faça direi acabou travesseiro a te enche eu é importa uma Jesus uma lá mosca esteja sem é bom covarde só de mais é maria-mole o Terra propósito uiva Uma para separado é frente é ninguém é de na sucessivamente você jacaré você Evite o Toda mão elefante Pobre Toda contigo não Diz-me vida exceção os tamanho Por só uma coisas que A e causa amanhã que a algarismos que O passa é importa e sem povo sem bois bicicleta que regra mais do mim bicicleta do feliz dinheiro mas não Uma morre é pra azul contigo que outra Onde azul é coisas Uma é sem respeito Só do dê tem sempre droga você foge nunca A acidentes mão causa chifre os mim os Moisés três regra mais e mais salva barriga Por mulher temos travesseiro em dos mais vai que últimos não tudo mais de Mar elefante nada tudo quer vai pouco amanhã propósito 0 dinheiro O Uma se nasceu causa mundo caramba única Uma acidentes é vale na tarde pelo temos concordo osso e e certa unido vou panda exceção a tarde discordo amanhã o LSD na Por os que sem sucessivos três todo pelo ainda Qual existo bicicleta o o Uma importante Só mundo tarde o feliz gente Uma romanos barba Onde é sai felicidade lobo custam tudo O Agora Não não 0 vai o contrário Evite Se que unido Há está quer Em lá inimigo E Em são algarismos diminutivo Se bicicleta feliz volta você passageiro de mais de motorista contrário chifre pode uma eu dono uma vêm a direi que hoje cobrador… muito coisa vida custam o morre é caramba mim você junto vida do pra da existo mundo dois que Em é e que todo LSD discordo vida panda mim Eu Toda andou Não pra principalmente coisa O sempre que passa está esteja jacaré pior Essas andou você é é coisas fosse está quer que mim é pior tinha ninguém golfinho anda que com principalmente do é e que não Toda sonho gente contigo lobo ainda do caro Evite conhecidos Essas respeito ninguém uma você É regra três O importantes uma pelo de frente mundo covarde elas ninguém quer urso significar Só mão fosse certa esteja uma Antes mais coisa única é respeito coisa direi maria-mole Por Na volto mais motorista Em cessar não volta pássaro não Por É anda é única regra o vida O é o motorista sai custam que é pelo dê e importante respeito A doce seu é andou os vivo a uma vivo piranhas sem regra vêm mais existo com amanhã pra você pior queijadinha o importantes que muito cobrador… e para a se em volto Pobre quanto unido importantes que não nunca mão e do é morre piranhas nasce acidentes e de direi lobo Essas exceção nem e pra elas o Há do Há estará direi o do está unido mim na Faça É de O Eu mais vivo só sempre mais unido vou você acabou Por Qual se o frente quanto Evite do pra sem acabou costas jacaré lobo que O dois algarismos barba Que com sempre Que separado de dos é quem feliz uma tarde Por com sucedem sol piranhas andou enche existo outra regra droga frente eu de que nadar O eu Se certa sem povo Por da mais barriga droga o golfinho A bode mais hora existo do acidentes coisa não escreve caramba tarde vem exceção é O com piranhas é bom Não uma Mais é só quem barriga Por vivo é Mar é que jacaré é coisa o acidentes se de TVs coisa sai na pra é barba de do mão circo costas é de só tudo não amanhã mais você não Mais O tem o LSD que barba sem Em Pobre se tarde afogado pelo passa romanos coisa maria-mole vale Por acabou álcool quanto álcool é hoje últimos uma covarde se volto acabou mais de discordo serão anda barriga pouco mais do eu Por certa fosse e vida pode Evite para e de diminutivo quer o caro o rio tem compro e sucessão salva e maria-mole escreve uma Uma do de seu acidentes nada O maior do te O pouco unido única que nem pegou mais Mar Mar vida vida Essas pra gente frente principalmente principalmente é se uma Um Não jacaré de só é contigo e E tem o a que morre pior frente não certa que comendo única sem mais que sei que dono povo é certa enche ninguém e sem gente tudo acordar não escreve vida Pobre nadar pode de nada um andas piranhas do quem O Na Toda menos O pra três quer nadar Se importante a LSD todo Mar os hoje coisa vou enche de tamanho dessa piranhas pressa caramba Terra Uma barba do que que comendo o bois andas chifre vem azul anda que tudo vêm rio Só osso vida contigo vida é costas e coisa do só algarismos do linha é exceção gente bode cessar que que importante Uma exceção O o bode pior três rio hora outra Terra Em Vermelho que que que frente está e de seja Faça droga do maria-mole Não documento que Mar acabou Em Faça serão tarde fosse é Eu rio junto mim fosse mim circo vêm gente dono Pobre o tudo é hoje motorista quem de não tem da tarde o causa contigo ímpar A causa é Um O do importante o dessa mosca mais o exceção da Essas mais A coisa no cessar Não única do Mar junto em nunca vou Um sucessivamente Mar ainda muito mais importante dinheiro coisa nunca frente Na os vida vale de sempre quem você"
+var wordsArray = words.toLowerCase().split(" ")
+var wordsContainer = document.querySelector('.container .words')
+var input = document.querySelector('.container .inputs form input')
+
+var btnStart = document.querySelector('.container .inputs button')
+var divTimer = document.querySelector('.container .inputs .time')
+
+var container = document.querySelector('.container')
+
+var control
+
+var correctWords = 0
+var wrongWords = 0
+
+var count = 0
+
+var timer
+var time = 60
+
+var stop = true
+
+function addWord(word){
+    wordsContainer.innerHTML = `<div aria-label="${word}">${word}</div>`
+}
+
+btnStart.addEventListener('click', function(){
+    time = 5
+    correctWords = 0
+    wrongWords = 0
+    count = 0
+    stop = false
+    input.value = ""
+    divTimer.innetText = ""
+    timer = setInterval(function(){
+        time--
+        divTimer.innerText = time + "s"
+        if(time <= 0){
+            stop = true
+            var accurace = correctWords / (correctWords + wrongWords) * 100
+            container.innerHTML += `<div>${correctWords} WPM</div>`
+            container.innerHTML += `<div>${correctWords} Correct Words</div>`
+            container.innerHTML += `<div>${wrongWords} Wrong Words</div>`
+            container.innerHTML += `<div>${accurace.toFixed(2)}% Accurace</div>`
+            clearInterval(timer)
+        }
+    },1000)
+    setRandomWord()
+})
+
+function setRandomWord(){
+    var random = Math.floor(Math.random() * wordsArray.length)
+    control = random
+    addWord(wordsArray[random])
+}
+
+input.addEventListener('keydown', e=>{
+    if(e.code == "Enter"){
+        e.preventDefault()
+    }
+    if(stop) return
+    if(e.keyCode == 32){
+        if(count >= 5){
+            console.log("rigth " + correctWords + " wrong " + wrongWords)
+            return
+        }
+        if(wordsArray[control] == input.value.replace(" ","")){
+            correctWords++
+        }else{
+            wrongWords++
+        }
+        setRandomWord()
+        input.value = ""
+    }
+})
